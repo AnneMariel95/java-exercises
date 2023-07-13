fcf7f07d408abdbecbf7af443970f1ba55abb8b4
AnneMariel95 <a.mariel.recio@gmail.com>
2023-07-13 20:31:24 +0200

    converted string to uppercase

---
+++ src/main/java/ToUpperCase.java
@@ -0,0 +1,10 @@
+public class ToUpperCase {
+    public static void main(String[] args) {
+
+        String txt = "hello there";
+
+        String txtup = txt.toUpperCase();
+
+        System.out.println(txtup);
+    }
+}
