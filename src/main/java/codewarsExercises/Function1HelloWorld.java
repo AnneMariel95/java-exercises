40ca754a02145f78652bb7efce7f4b58a8d05a4a
AnneMariel95 <a.mariel.recio@gmail.com>
2023-07-14 21:07:39 +0200

    Did the most famous hello world exercise

---
+++ src/main/java/codewarsExercises/Function1HelloWorld.java
@@ -0,0 +1,13 @@
+package codewarsExercises;
+
+// Write a public static function `greet` that returns "hello world!"
+
+public class Function1HelloWorld {
+    public static String greet() {
+        return "hello world!";
+    }
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+}
+
