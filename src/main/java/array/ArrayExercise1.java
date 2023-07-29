777b0497d5c6a29cf4b6ca52eedb464d34a6f039
AnneMariel95 <a.mariel.recio@gmail.com>
2023-07-29 21:03:34 +0200

    extract methods to simplify main

--- src/main/java/array/ArrayExercise1.java
+++ src/main/java/array/ArrayExercise1.java
@@ -1,36 +1,49 @@
 package array;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayExercise1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int quantity = getQuantity();
 
-        System.out.println("Enter number of integer:");
-        int Quantity = scanner.nextInt();
-
-        if (Quantity <= 3 || Quantity >= 9) {
+        if (quantity <= 3 || quantity >= 9) {
             System.out.println("Out of range. Try again.");
+            return;
         }
 
-        String enter = "Enter integer #: ";
-        int[] NumbersArray = new int[Quantity];
+        int[] NumbersArray = getIntegers(quantity);
+
+        System.out.println(calculateSum(NumbersArray));
+    }
+
+    private static int calculateSum(int[] integers) {
         int sum = 0;
 
+        for (int i = 0; i < integers.length; i++) {
+            sum = sum + integers[i];
+        }
 
-        for (int i = 0; i < Quantity; i++) {
+        return sum;
+    }
+
+    private static int getQuantity() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of integer:");
+        return scanner.nextInt();
+    }
+
+    private static int[] getIntegers(int size) {
+        Scanner scanner = new Scanner(System.in);
+        String enter = "Enter integer #: ";
+
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
             System.out.println(enter + (i + 1));
             int num = scanner.nextInt();
-            NumbersArray[i] = num;
+            array[i] = num;
         }
 
-        for (int i = 0; i < Quantity; i++) {
-            sum = sum + NumbersArray[i];
-        }
-        System.out.println(sum);
-
-        scanner.close();
+        return array;
     }
 }
 
