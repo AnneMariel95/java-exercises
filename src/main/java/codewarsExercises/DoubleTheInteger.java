8b1985fe785d783d20b726a757891279c4689b3d
AnneMariel95 <a.mariel.recio@gmail.com>
2023-08-27 20:21:33 +0200

    Double the integer

---
+++ src/main/java/codewarsExercises/DoubleTheInteger.java
@@ -0,0 +1,7 @@
+package codewarsExercises;
+
+public class DoubleTheInteger {
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
---
+++ src/test/java/codewarsExercises/DoubleTheIntegerTest.java
@@ -0,0 +1,12 @@
+package codewarsExercises;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DoubleTheIntegerTest {
+    @Test
+    public final void test_all() {
+        assertEquals(4, DoubleTheInteger.doubleInteger(2)); // Test cases here!
+    }
+}
